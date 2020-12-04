d3bc379c673ea06cc0becdc2d686913a6f55ac76
Brian Conway <bconway@rcesoftware.com>
2020-12-04 14:51:34 -0600

    Add bytes to DataPoint and regenerate

--- python/src/thingspect_api/mqtt/mqtt_pb2.py
+++ python/src/thingspect_api/mqtt/mqtt_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z!github.com/thingspect/api/go/mqtt',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x0fmqtt/mqtt.proto\x12\x04mqtt\x1a\x1fgoogle/protobuf/timestamp.proto\"\xa1\x02\n\tDataPoint\x12\x17\n\x07uniq_id\x18\x01 \x01(\tR\x06uniqID\x12\x0c\n\x04\x61ttr\x18\x02 \x01(\t\x12\x11\n\x07int_val\x18\x03 \x01(\x12H\x00\x12\x12\n\x08\x66l64_val\x18\x04 \x01(\x01H\x00\x12\x11\n\x07str_val\x18\x05 \x01(\tH\x00\x12\x12\n\x08\x62ool_val\x18\x06 \x01(\x08H\x00\x12,\n\x07map_val\x18\x10 \x03(\x0b\x32\x1b.mqtt.DataPoint.MapValEntry\x12&\n\x02ts\x18\x07 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\r\n\x05token\x18\x08 \x01(\t\x1a-\n\x0bMapValEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x0b\n\tval_oneof\"9\n\x07Payload\x12\x1f\n\x06points\x18\x01 \x03(\x0b\x32\x0f.mqtt.DataPoint\x12\r\n\x05token\x18\x02 \x01(\tB#Z!github.com/thingspect/api/go/mqttb\x06proto3'
+  serialized_pb=b'\n\x0fmqtt/mqtt.proto\x12\x04mqtt\x1a\x1fgoogle/protobuf/timestamp.proto\"\xb6\x02\n\tDataPoint\x12\x17\n\x07uniq_id\x18\x01 \x01(\tR\x06uniqID\x12\x0c\n\x04\x61ttr\x18\x02 \x01(\t\x12\x11\n\x07int_val\x18\x03 \x01(\x12H\x00\x12\x12\n\x08\x66l64_val\x18\x04 \x01(\x01H\x00\x12\x11\n\x07str_val\x18\x05 \x01(\tH\x00\x12\x12\n\x08\x62ool_val\x18\x06 \x01(\x08H\x00\x12\x13\n\tbytes_val\x18\x10 \x01(\x0cH\x00\x12,\n\x07map_val\x18\x11 \x03(\x0b\x32\x1b.mqtt.DataPoint.MapValEntry\x12&\n\x02ts\x18\x07 \x01(\x0b\x32\x1a.google.protobuf.Timestamp\x12\r\n\x05token\x18\x08 \x01(\t\x1a-\n\x0bMapValEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x0b\n\tval_oneof\"9\n\x07Payload\x12\x1f\n\x06points\x18\x01 \x03(\x0b\x32\x0f.mqtt.DataPoint\x12\r\n\x05token\x18\x02 \x01(\tB#Z!github.com/thingspect/api/go/mqttb\x06proto3'
   ,
   dependencies=[google_dot_protobuf_dot_timestamp__pb2.DESCRIPTOR,])
 
@@ -61,8 +61,8 @@ _DATAPOINT_MAPVALENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=290,
-  serialized_end=335,
+  serialized_start=311,
+  serialized_end=356,
 )
 
 _DATAPOINT = _descriptor.Descriptor(
@@ -116,21 +116,28 @@ _DATAPOINT = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='map_val', full_name='mqtt.DataPoint.map_val', index=6,
-      number=16, type=11, cpp_type=10, label=3,
+      name='bytes_val', full_name='mqtt.DataPoint.bytes_val', index=6,
+      number=16, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"",
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='map_val', full_name='mqtt.DataPoint.map_val', index=7,
+      number=17, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='ts', full_name='mqtt.DataPoint.ts', index=7,
+      name='ts', full_name='mqtt.DataPoint.ts', index=8,
       number=7, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='token', full_name='mqtt.DataPoint.token', index=8,
+      name='token', full_name='mqtt.DataPoint.token', index=9,
       number=8, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
@@ -154,7 +161,7 @@ _DATAPOINT = _descriptor.Descriptor(
     fields=[]),
   ],
   serialized_start=59,
-  serialized_end=348,
+  serialized_end=369,
 )
 
 
@@ -192,8 +199,8 @@ _PAYLOAD = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=350,
-  serialized_end=407,
+  serialized_start=371,
+  serialized_end=428,
 )
 
 _DATAPOINT_MAPVALENTRY.containing_type = _DATAPOINT
@@ -211,6 +218,9 @@ _DATAPOINT.fields_by_name['str_val'].containing_oneof = _DATAPOINT.oneofs_by_nam
 _DATAPOINT.oneofs_by_name['val_oneof'].fields.append(
   _DATAPOINT.fields_by_name['bool_val'])
 _DATAPOINT.fields_by_name['bool_val'].containing_oneof = _DATAPOINT.oneofs_by_name['val_oneof']
+_DATAPOINT.oneofs_by_name['val_oneof'].fields.append(
+  _DATAPOINT.fields_by_name['bytes_val'])
+_DATAPOINT.fields_by_name['bytes_val'].containing_oneof = _DATAPOINT.oneofs_by_name['val_oneof']
 _PAYLOAD.fields_by_name['points'].message_type = _DATAPOINT
 DESCRIPTOR.message_types_by_name['DataPoint'] = _DATAPOINT
 DESCRIPTOR.message_types_by_name['Payload'] = _PAYLOAD
